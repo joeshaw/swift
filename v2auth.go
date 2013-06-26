4af15dfc0c5e4b0aff33b23987b20eea450a0720
Nick Craig-Wood <nick@craig-wood.com>
2013-06-26 13:46:08 +0100

    Implement v2 auth to enable endpoint choice - fixes #1

---
+++ v2auth.go
@@ -0,0 +1,78 @@
+package swift
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+// V2 Authentication request
+//
+// http://docs.openstack.org/developer/keystone/api_curl_examples.html
+// http://docs.rackspace.com/servers/api/v2/cs-gettingstarted/content/curl_auth.html
+type V2AuthRequest struct {
+	Auth struct {
+		ApiKeyCredentials struct {
+			UserName string `json:"username"`
+			ApiKey   string `json:"apiKey"`
+		} `json:"RAX-KSKEY:apiKeyCredentials"`
+		PasswordCredentials struct {
+			UserName string `json:"username"`
+			Password string `json:"password"`
+		} `json:"passwordCredentials"`
+	} `json:"auth"`
+}
+
+// V2 Authentication reply
+//
+// http://docs.openstack.org/developer/keystone/api_curl_examples.html
+// http://docs.rackspace.com/servers/api/v2/cs-gettingstarted/content/curl_auth.html
+type V2Auth struct {
+	Access struct {
+		ServiceCatalog []struct {
+			Endpoints []struct {
+				InternalUrl string
+				PublicUrl   string
+				Region      string
+				TenantId    string
+			}
+			Name string
+			Type string
+		}
+		Token struct {
+			Expires string
+			Id      string
+			Tenant  struct {
+				id   string
+				name string
+			}
+		}
+		User struct {
+			DefaultRegion string `json:"RAX-AUTH:defaultRegion"`
+			Id            string
+			Name          string
+			Roles         []struct {
+				Description string
+				Id          string
+				Name        string
+				TenantId    string
+			}
+		}
+	}
+}
+
+// Create a V2 auth request for the body of the connection
+//
+// Adds both ApiKey and Password auth
+func NewV2AuthRequest(UserName, ApiKey, Password string) (io.Reader, error) {
+	v2 := V2AuthRequest{}
+	v2.Auth.ApiKeyCredentials.UserName = UserName
+	v2.Auth.ApiKeyCredentials.ApiKey = ApiKey
+	v2.Auth.PasswordCredentials.UserName = UserName
+	v2.Auth.PasswordCredentials.Password = Password
+	body, err := json.Marshal(v2)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBuffer(body), nil
+}
